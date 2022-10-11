4522b5bef55074fdad4a3baa095ad7392b735cce
fangchi <fangchi@trustance.net>
2022-10-11 18:04:13 +0800

    1.加部份圖

--- lib/views/login/login_main_view.dart
+++ lib/views/login/login_main_view.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:treasure_nft_project/constant/theme/app_image_path.dart';
 import 'package:treasure_nft_project/constant/ui_define.dart';
 import 'package:treasure_nft_project/view_models/login/login_main_viewmodel.dart';
 import 'package:treasure_nft_project/widgets/text_field/login_text_widget.dart';
@@ -35,24 +36,34 @@ class _LoginMainViewState extends State<LoginMainView> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const DomainBar(),
-        Text(tr('account'),
-            style: TextStyle(
-                fontWeight: FontWeight.bold, fontSize: UIDefine.fontSize14)),
-        LoginTextWidget(
-            hintText: tr('placeholder-account\''),
-            controller: accountController),
-        ErrorTextWidget(data: viewModel.accountData),
-        Text(tr('password'),
-            style: TextStyle(
-                fontWeight: FontWeight.bold, fontSize: UIDefine.fontSize14)),
-        LoginTextWidget(
-            hintText: tr('placeholder-password'),
-            controller: passwordController),
-        ErrorTextWidget(data: viewModel.passwordData),
+        _buildTop(),
+        _buildBottom(),
       ],
     );
+  }
+
+  Widget _buildTop() {
+    return Stack(
+      children: [Image.asset(AppImagePath.loginBg)],
+    );
+  }
+
+  Widget _buildBottom() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Text(tr('account'),
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: UIDefine.fontSize14)),
+      LoginTextWidget(
+          hintText: tr('placeholder-account\''), controller: accountController),
+      ErrorTextWidget(data: viewModel.accountData),
+      Text(tr('password'),
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: UIDefine.fontSize14)),
+      LoginTextWidget(
+          hintText: tr('placeholder-password'), controller: passwordController),
+      ErrorTextWidget(data: viewModel.passwordData)
+    ]);
   }
 }
