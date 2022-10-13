201ac900d8003235ed9245b08d952284b5738636
fangchi <fangchi@trustance.net>
2022-10-13 17:27:18 +0800

    1.登入成功視窗

--- lib/view_models/login/login_main_viewmodel.dart
+++ lib/view_models/login/login_main_viewmodel.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:treasure_nft_project/constant/theme/app_animation_path.dart';
 import 'package:treasure_nft_project/models/data/validate_result_data.dart';
 import 'package:treasure_nft_project/models/http/api/login_api.dart';
 import 'package:treasure_nft_project/view_models/base_view_model.dart';
@@ -11,6 +12,7 @@ import '../../constant/global_data.dart';
 import '../../views/login/forgot_main_page.dart';
 import '../../views/login/register_main_page.dart';
 import '../../views/main_page.dart';
+import '../../widgets/dialog/animation_dialog.dart';
 import '../../widgets/dialog/simple_custom_dialog.dart';
 
 class LoginMainViewModel extends BaseViewModel {
@@ -52,11 +54,11 @@ class LoginMainViewModel extends BaseViewModel {
               account: accountController.text,
               password: passwordController.text)
           .then((value) async {
-        value.printLog();
+
         await saveUserLoginInfo(response: value);
         GlobalData.mainBottomType = AppNavigationBarType.typeMain;
         pushAndRemoveUntil(context, MainPage(type: GlobalData.mainBottomType));
-        SimpleCustomDialog(context, mainText: 'OK').show();
+        SimpleCustomDialog(context).show();
       });
     }
   }
--- lib/widgets/dialog/animation_dialog.dart
+++ lib/widgets/dialog/animation_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:lottie/lottie.dart';
 import 'package:treasure_nft_project/widgets/dialog/base_dialog.dart';
