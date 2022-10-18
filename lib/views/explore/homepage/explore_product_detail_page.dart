49a9f63a670cc2f117673bd599b5a2d04b39e520
Ethan <jj22584992@gmail.com>
2022-10-18 15:09:35 +0800

    1.倒數計時Util   2.折線圖Util   3.單一商品頁完成

--- lib/views/explore/homepage/explore_product_detail_page.dart
+++ lib/views/explore/homepage/explore_product_detail_page.dart
@@ -1,14 +1,17 @@
-import 'package:charts_flutter/flutter.dart' as chart;
+import 'dart:async';
+
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:treasure_nft_project/constant/global_data.dart';
 import 'package:treasure_nft_project/constant/theme/app_colors.dart';
 
 import '../../../constant/ui_define.dart';
+import '../../../utils/timer_util.dart';
 import '../../../view_models/base_view_model.dart';
 import '../../../view_models/explore/explore_product_detail_view_model.dart';
 import '../../../widgets/app_bottom_navigation_bar.dart';
 import '../../../widgets/appbar/custom_app_bar.dart';
+import '../../explore_chart_view.dart';
 import '../data/explore_item_response_data.dart';
 
 class ExploreProductDetailPage extends StatefulWidget {
@@ -32,19 +35,40 @@ class _ExploreProductDetailPage extends State<ExploreProductDetailPage> {
   ExploreItemResponseData data = ExploreItemResponseData(priceHistory: [], sellTimeList: []);
   List<SellTimeList> sellTimeList = [];
   List<PriceHistory> priceHistoryList = [];
+  String sTimeLeft = '00:00:00:00';
+  Widget chartView = const ExploreChartView(priceHistoryList: [], todayPrice: '',);
+  late Timer timer;
 
   @override
   void initState() {
     super.initState();
     Future<ExploreItemResponseData> resList = viewModel.getExploreItemDetail(widget.itemId);
     resList.then((value) => _setData(value));
+
+    _timer();
+  }
+
+  @override
+  void dispose() {
+    CountDownTimerUtil().cancelTimer();
+    super.dispose();
   }
 
   _setData(value) {
     data = value;
     sellTimeList = data.sellTimeList;
     priceHistoryList = data.priceHistory;
+    chartView = ExploreChartView(priceHistoryList: priceHistoryList, todayPrice: data.price);
     setState(() {});
+  }
+
+  _timer() {
+    CountDownTimerUtil().init(callBackListener:
+    MyCallBackListener(
+        myCallBack: (sTimeLeft) {
+      setState(() { this.sTimeLeft = sTimeLeft; });
+    }),
+        endTimeSeconds: 0);
   }
 
   @override
@@ -67,35 +91,38 @@ class _ExploreProductDetailPage extends State<ExploreProductDetailPage> {
             ),
 
             /// 提醒標語+倒數計時
-            Padding(
-              padding: EdgeInsets.fromLTRB(UIDefine.getScreenWidth(5), UIDefine.getScreenWidth(10),
-                  UIDefine.getScreenWidth(5), UIDefine.getScreenWidth(0)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Auction will being in',
-                    style: TextStyle(fontSize: UIDefine.fontSize18, fontWeight: FontWeight.w500),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(6, 6, 6, 6),
-                    decoration: BoxDecoration(
-                        color: AppColors.mainThemeButton,
-                        borderRadius: BorderRadius.circular(6)
+            Visibility(
+              visible: sellTimeList.isNotEmpty,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(UIDefine.getScreenWidth(5), UIDefine.getScreenWidth(10),
+                    UIDefine.getScreenWidth(5), UIDefine.getScreenWidth(0)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Auction will being in',
+                      style: TextStyle(fontSize: UIDefine.fontSize18, fontWeight: FontWeight.w500),
                     ),
-                    child: Row(
-                      children: [
-                        Image.asset('assets/icon/icon/icon_clock_01.png'),
-                        const SizedBox(width: 4),
-                        Text(
-                          '00:01:00:20',
-                          style: TextStyle(color: Colors.white, fontSize: UIDefine.fontSize14, fontWeight: FontWeight.w700),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(6, 6, 6, 6),
+                      decoration: BoxDecoration(
+                          color: AppColors.mainThemeButton,
+                          borderRadius: BorderRadius.circular(6)
+                      ),
+                      child: Row(
+                        children: [
+                          Image.asset('assets/icon/icon/icon_clock_01.png'),
+                          const SizedBox(width: 4),
+                          Text(
+                            sTimeLeft,
+                            style: TextStyle(color: Colors.white, fontSize: UIDefine.fontSize14, fontWeight: FontWeight.w700),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
             ),
 
             /// 商品名+詳細資訊
@@ -124,8 +151,8 @@ class _ExploreProductDetailPage extends State<ExploreProductDetailPage> {
             Image.network(data.imgUrl, width: UIDefine.getScreenWidth(100), height: UIDefine.getScreenWidth(100))
             : Container(),
 
-            /// 折線圖
-            Container( // 標題
+            /// 折線圖標題
+            Container(
               alignment: Alignment.center,
               padding: EdgeInsets.all(UIDefine.getScreenWidth(5.5)),
               child: Text(
@@ -134,68 +161,8 @@ class _ExploreProductDetailPage extends State<ExploreProductDetailPage> {
               ),
             ),
 
-            Container( // 折線圖
-              padding: EdgeInsets.fromLTRB(UIDefine.getScreenWidth(5), 0, UIDefine.getScreenWidth(5), 0),
-              width: UIDefine.getScreenWidth(100),
-              height: UIDefine.getScreenWidth(55),
-
-              child: AbsorbPointer(
-                absorbing: true, // Disable折線圖的click 不然會影響scroll
-                child: chart.LineChart(
-                    _setChartData(),
-                    animate: true,
-                    primaryMeasureAxis: chart.NumericAxisSpec(
-                        showAxisLine: true,
-                        tickProviderSpec: chart.StaticNumericTickProviderSpec(
-                          priceHistoryList.isNotEmpty?
-                          <chart.TickSpec<num>> [ // 可另外寫Func找出最低最高點 用來設表格上下限 (這裡是劃出橫向界線)
-                            chart.TickSpec<num>(double.parse(priceHistoryList[0].price)),
-                            chart.TickSpec<num>(double.parse(priceHistoryList[1].price)),
-                            chart.TickSpec<num>(double.parse(priceHistoryList[2].price)),
-                            chart.TickSpec<num>(double.parse(priceHistoryList[3].price)),
-                            chart.TickSpec<num>(double.parse(data.price)),
-                            chart.TickSpec<num>(double.parse(priceHistoryList[3].price) - (double.parse(priceHistoryList[3].price)/10)),
-                            chart.TickSpec<num>(double.parse(priceHistoryList[3].price) + (double.parse(priceHistoryList[3].price)/10)),
-                          ]
-                              :
-                          <chart.TickSpec<num>> []
-                        ),
-                        renderSpec: chart.GridlineRendererSpec(
-                            labelStyle: chart.TextStyleSpec(
-                                fontSize: 14, // size in Pts.
-                                color: chart.Color.fromHex(code: '#7A7C7D')),
-
-                            lineStyle: chart.LineStyleSpec(
-                                thickness: 1,
-                                color: chart.Color.fromHex(code: '#7A7C7D'))
-                      )
-                  ),
-
-                  domainAxis: chart.NumericAxisSpec(
-                      tickProviderSpec: const chart.BasicNumericTickProviderSpec(desiredTickCount:7),
-                      tickFormatterSpec: customTickFormatter,
-                      showAxisLine: false,
-                      renderSpec: chart.GridlineRendererSpec(
-                          labelStyle: chart.TextStyleSpec(
-                              fontSize: 14, // size in Pts.
-                              color: chart.Color.fromHex(code: '#7A7C7D')),
-
-                          lineStyle: const chart.LineStyleSpec(
-                              thickness: 0,
-                              color: chart.MaterialPalette.transparent)
-                      )
-                  ),
-
-                  defaultRenderer: chart.LineRendererConfig(
-                    radiusPx: 4,
-                    stacked: false,
-                    strokeWidthPx: 4,
-                    includeLine: true,
-                    includePoints: true,
-                  )
-                )
-              )
-            ),
+            /// 折線圖圖表
+            chartView,
 
             /// 預約按鈕+預約券總數量
             Container(
@@ -269,55 +236,12 @@ class _ExploreProductDetailPage extends State<ExploreProductDetailPage> {
                 ],
               )
             )
-
           ],
         ),
       ),
 
       bottomNavigationBar: const AppBottomNavigationBar(initType: AppNavigationBarType.typeExplore),
     );
-  }
-
-  /// 將橫軸名稱轉換為日期
-  late chart.BasicNumericTickFormatterSpec customTickFormatter = chart.BasicNumericTickFormatterSpec((value) {
-    if (value == 1) {
-      return _getLastFourDate(priceHistoryList[0].date);
-    } else if (value == 2) {
-      return _getLastFourDate(priceHistoryList[1].date);
-    } else if (value == 3) {
-      return _getLastFourDate(priceHistoryList[2].date);
-    } else if (value == 4) {
-      return _getLastFourDate(priceHistoryList[3].date);
-    } else if (value == 5) {
-      return _getMonthDate(data.price);
-    }
-    return '';
-  });
-
-  /// X,Y軸的數值設置
-  _setChartData() {
-    List<chart.Series<dynamic, num>> seriesList = [];
-    if (priceHistoryList.isNotEmpty) {
-      var data = [
-        ChartData(1, double.parse(priceHistoryList[0].price)),
-        ChartData(2, double.parse(priceHistoryList[1].price)),
-        ChartData(3, double.parse(priceHistoryList[2].price)),
-        ChartData(4, double.parse(priceHistoryList[3].price)),
-        ChartData(5, double.parse(this.data.price)),
-      ];
-
-      seriesList = [
-        chart.Series<ChartData, int>(
-          id: 'Sales',
-          colorFn: (_, __) => chart.MaterialPalette.green.shadeDefault,
-          domainFn: (ChartData sales, _) => sales.date,
-          measureFn: (ChartData sales, _) => sales.price,
-          data: data,
-        )
-      ];
-      setState(() {});
-    }
-    return seriesList;
   }
 
   List<String> _getTimeZoneList() {
@@ -370,18 +294,6 @@ class _ExploreProductDetailPage extends State<ExploreProductDetailPage> {
       ));
     }
     return textView;
-  }
-
-  /// 將今天的日期轉為HH/dd 顯示
-  String _getMonthDate(String sValue) {
-    DateFormat dateFormat = DateFormat("MM-dd");
-    String sDate = dateFormat.format(DateTime.now());
-    return sDate.replaceAll('-', '/');
-  }
-
-  /// 將response的四個日期轉為HH/dd 顯示
-  String _getLastFourDate(String sValue) {
-    return sValue.substring(5, 10).replaceAll('-', '/');
   }
 
 }
