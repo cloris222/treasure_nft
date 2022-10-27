37fadca47763a127d4ef9ef69bada545194810db
fangchi <fangchi@trustance.net>
2022-10-27 10:06:03 +0800

    1.調整登入UI 加入清除data的判斷

--- lib/views/sigin_in_page.dart
+++ lib/views/sigin_in_page.dart
@@ -1,6 +1,10 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:treasure_nft_project/constant/global_data.dart';
+import 'package:treasure_nft_project/view_models/base_view_model.dart';
 
 import '../models/http/parameter/sign_in_data.dart';
+import '../widgets/button/action_button_widget.dart';
 
 ///MARK: 每日簽到
 class SignInPage extends StatelessWidget {
@@ -10,17 +14,28 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+        body: SingleChildScrollView(
+            child: Column(children: [
+      Container(
         color: Colors.white,
         child: ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
           itemBuilder: _buildItem,
           itemCount: data.finishedDateList.length,
         ),
       ),
-    );
+      ActionButtonWidget(
+          btnText: tr('checkin'), onPressed: () => _onPressSignIn(context))
+    ])));
   }
 
   Widget _buildItem(BuildContext context, int index) {
     return Text(data.finishedDateList[index]);
+  }
+
+  void _onPressSignIn(BuildContext context) {
+    GlobalData.signInInfo = null;
+    BaseViewModel().popPage(context);
   }
 }
