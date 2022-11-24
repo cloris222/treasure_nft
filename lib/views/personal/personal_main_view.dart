c5e6181ad6471744598373f273ee2b7d79a84e95
fangchi <fangchi@trustance.net>
2022-11-24 09:54:49 +0800

    個人首頁的細線調整

--- lib/views/personal/personal_main_view.dart
+++ lib/views/personal/personal_main_view.dart
@@ -73,7 +73,7 @@ class _PersonalMainViewState extends State<PersonalMainView> {
   }
 
   Widget _buildLine() {
-    return const Divider(color: AppColors.searchBar);
+    return const Divider(color: AppColors.searchBar, thickness: 1);
   }
 
   void _onViewUpdate() {
