b321fe7e2b0869f66d6345178725d72a8f5230aa
Ethan <jj22584992@gmail.com>
2022-12-12 16:50:49 +0800

    v0.0.6 _ 交易主頁UI調整

--- lib/widgets/trade_countdown_view.dart
+++ lib/widgets/trade_countdown_view.dart
@@ -1,6 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 import '../constant/enum/trade_enum.dart';
 import '../constant/global_data.dart';
@@ -29,96 +31,106 @@ class _TradeCountDownViewState extends State<TradeCountDownView> {
   @override
   Widget build(BuildContext context) {
     TradeData tradeData = widget.tradeData;
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Image.asset(
-          AppImagePath.countDownBackground,
-          width: UIDefine.getWidth(),
-          height: UIDefine.getWidth(),
-          fit: BoxFit.cover,
-        ),
-        Container(
-          margin: EdgeInsets.only(top: UIDefine.getHeight() / 25),
-          width: UIDefine.getWidth() / 1.1,
-          height: UIDefine.getWidth() / 1.3,
-          decoration: AppStyle().styleColorBorderBackground(
-              color: Colors.white,
-              backgroundColor: Colors.transparent,
-              borderLine: 2),
-        ),
-        Container(
-          margin: EdgeInsets.only(top: UIDefine.getHeight() / 25),
-          width: UIDefine.getWidth() / 1.2,
-          height: UIDefine.getWidth() / 1.45,
-          decoration: AppStyle().styleColorsRadiusBackground(
-              color: Colors.white.withOpacity(0.5)),
-        ),
-        Positioned(
-          top: UIDefine.getHeight() / 8,
-          child: Column(
-            children: [
-              Image.asset(
-                tradeData.status == SellingState.NotYet
-                    ? AppImagePath.clockBlue
-                    : AppImagePath.clockRed,
-                width: UIDefine.getWidth() / 3.6,
-                height: UIDefine.getWidth() / 3.6,
-                fit: BoxFit.contain,
-              ),
-              SizedBox(
-                height: UIDefine.getHeight() / 50,
-              ),
-              tradeData.status == SellingState.Selling
-                  ? Text(
-                      tr('onSale'),
-                      style: TextStyle(
-                          color: AppColors.textRed,
-                          fontSize: UIDefine.fontSize24,
-                          fontWeight: FontWeight.w500),
-                    )
-                  : CountDownTimer(
-                      duration: tradeData.duration,
-                    ),
-              LoginButtonWidget(
-                width: UIDefine.getWidth() / 1.7,
-                height: UIDefine.getHeight() / 20,
-                btnText:
-                    '(${GlobalData.userInfo.zone}) ${DateFormatUtil().getDateWith12HourInSecondFormat(TradeTimerUtil().getSellStartTime())}',
-                fontSize: UIDefine.fontSize14,
-                fontWeight: FontWeight.w500,
-                onPressed: () {},
-              )
-            ],
+    return Container(
+      width: UIDefine.getWidth(),
+      padding: EdgeInsets.only(top: UIDefine.getScreenWidth(5), bottom: UIDefine.getScreenWidth(8)),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+        image: AssetImage(AppImagePath.countDownBackground),
+        fit: BoxFit.cover,
+        )
+      ),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(right: UIDefine.getScreenWidth(4.5)),
+            alignment: Alignment.centerRight,
+            child: _ruleAction(context)
           ),
-        ),
-        _ruleAction(context)
-      ],
+
+          SizedBox(height: UIDefine.getScreenWidth(5)),
+
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                // margin: EdgeInsets.only(top: UIDefine.getHeight() / 25),
+                width: UIDefine.getWidth() / 1.1,
+                height: UIDefine.getWidth() / 1.3,
+                decoration: AppStyle().styleColorBorderBackground(
+                    color: Colors.white,
+                    backgroundColor: Colors.transparent,
+                    borderLine: 2),
+              ),
+              Container(
+                // margin: EdgeInsets.only(top: UIDefine.getHeight() / 25),
+                width: UIDefine.getWidth() / 1.2,
+                height: UIDefine.getWidth() / 1.45,
+                decoration: AppStyle().styleColorsRadiusBackground(
+                    color: Colors.white.withOpacity(0.5)),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    tradeData.status == SellingState.NotYet
+                        ? AppImagePath.clockBlue
+                        : AppImagePath.clockRed,
+                    width: UIDefine.getWidth() / 3.6,
+                    height: UIDefine.getWidth() / 3.6,
+                    fit: BoxFit.contain,
+                  ),
+                  SizedBox(
+                    height: UIDefine.getHeight() / 50,
+                  ),
+                  tradeData.status == SellingState.Selling
+                      ? Text(
+                    tr('onSale'),
+                    style: TextStyle(
+                        color: AppColors.textRed,
+                        fontSize: UIDefine.fontSize24,
+                        fontWeight: FontWeight.w500),
+                  )
+                      : CountDownTimer(
+                    duration: tradeData.duration,
+                  ),
+                  LoginButtonWidget(
+                    width: UIDefine.getWidth() / 1.7,
+                    height: UIDefine.getHeight() / 20,
+                    btnText:
+                    '(${GlobalData.userInfo.zone}) ${DateFormatUtil().getDateWith12HourInSecondFormat(TradeTimerUtil().getSellStartTime())}',
+                    fontSize: UIDefine.fontSize14,
+                    fontWeight: FontWeight.w500,
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 
   Widget _ruleAction(BuildContext context) {
-    return Positioned(
-        top: UIDefine.getHeight()/50,
-        right: 10,
-        child: InkWell(
-          onTap: () {
-            TradeRuleDialog(context).show();
-          },
-          child: Container(
-            padding: const EdgeInsets.all(2),
-            decoration: AppStyle().styleColorBorderBackground(
-              radius: 7,
-              color: Colors.black,
-              backgroundColor: Colors.transparent,
-              borderLine: 2,
-            ),
-            child: Text(
-              tr('trade-rules'),
-              style: TextStyle(
-                  fontWeight: FontWeight.w500, fontSize: UIDefine.fontSize14),
-            ),
-          ),
-        ));
+    return InkWell(
+      onTap: () {
+        TradeRuleDialog(context).show();
+      },
+      child: Container(
+        padding: const EdgeInsets.all(2),
+        decoration: AppStyle().styleColorBorderBackground(
+          radius: 7,
+          color: Colors.black,
+          backgroundColor: Colors.transparent,
+          borderLine: 2,
+        ),
+        child: Text(
+          tr('trade-rules'),
+          style: TextStyle(
+              fontWeight: FontWeight.w500, fontSize: UIDefine.fontSize14),
+        ),
+      ),
+    );
   }
 }
