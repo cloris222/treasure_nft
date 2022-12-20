def5572263f2da5eba33df89377c592f218a547f
Ethan <jj22584992@gmail.com>
2022-12-20 19:46:32 +0800

    v0.0.7 _ 尚缺背景圖 + iCon

---
+++ lib/views/personal/personal_new_sub_common_view.dart
@@ -0,0 +1,84 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+import '../../constant/call_back_function.dart';
+import '../../constant/theme/app_colors.dart';
+import '../../constant/theme/app_image_path.dart';
+import '../../constant/ui_define.dart';
+import '../../view_models/base_view_model.dart';
+import '../../widgets/label/personal_param_item.dart';
+import 'common/user_create_page.dart';
+import 'common/user_novice_page.dart';
+import 'common/user_setting_page.dart';
+
+class PersonalNewSubCommonView extends StatelessWidget {
+  const PersonalNewSubCommonView({super.key, required this.onViewUpdate});
+  final onClickFunction onViewUpdate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      color: AppColors.textWhite,
+      child: Container(
+        padding: EdgeInsets.all(UIDefine.getScreenWidth(3.5)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(tr('usualFun'), // 標題 常用功能
+                style: TextStyle(color: AppColors.dialogBlack,
+                    fontSize: UIDefine.fontSize20, fontWeight: FontWeight.w500)),
+
+            _getLine(),
+
+            Row(
+              children: [
+                Flexible(
+                  child: PersonalParamItem(
+                      title: tr('uc_novice'),
+                      assetImagePath: AppImagePath.userNoviceIcon,
+                      onPress: () => _showUserNovicePage(context))),
+                Flexible(
+                  child: PersonalParamItem(
+                      title: tr('paymentSetting'),
+                      assetImagePath: AppImagePath.userSettingIcon,
+                      onPress: () => _showUserSettingPage(context))),
+                Platform.isIOS
+                  ? const SizedBox()
+                  : Flexible(
+                  child: PersonalParamItem(
+                      title: tr('create'),
+                      assetImagePath: AppImagePath.userCreateIcon,
+                      onPress: () => _showUserCreatePage(context)))
+            ])
+          ],
+        ),
+      )
+    );
+  }
+
+  Widget _getLine() {
+    return Container(
+        margin: EdgeInsets.symmetric(vertical: UIDefine.getScreenWidth(2.7)),
+        width: double.infinity,
+        height: 1,
+        color: AppColors.searchBar
+    );
+  }
+
+  void _showUserNovicePage(BuildContext context) {
+    BaseViewModel().pushPage(context, const UserNovicePage());
+  }
+
+  void _showUserSettingPage(BuildContext context) async {
+    await BaseViewModel().pushPage(context, const UserSettingPage());
+    onViewUpdate();
+  }
+
+  void _showUserCreatePage(BuildContext context) {
+    BaseViewModel().pushPage(context, const UserCreatePage());
+  }
+
+}
---
+++ lib/views/personal/personal_new_sub_team_view.dart
@@ -0,0 +1,120 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:treasure_nft_project/views/personal/team/team_contribution_page.dart';
+import 'package:treasure_nft_project/views/personal/team/team_member_page.dart';
+import 'package:treasure_nft_project/views/personal/team/team_order_page.dart';
+import 'package:treasure_nft_project/views/personal/team/team_referral_code_page.dart';
+
+import '../../constant/theme/app_colors.dart';
+import '../../constant/theme/app_image_path.dart';
+import '../../constant/ui_define.dart';
+import '../../models/http/parameter/check_level_info.dart';
+import '../../utils/number_format_util.dart';
+import '../../view_models/base_view_model.dart';
+import '../../widgets/label/personal_param_item.dart';
+
+class PersonalNewSubTeamView extends StatelessWidget {
+  const PersonalNewSubTeamView({super.key, this.levelInfo});
+  final CheckLevelInfo? levelInfo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      color: AppColors.textWhite,
+      child: Container(
+        padding: EdgeInsets.all(UIDefine.getScreenWidth(3.5)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(tr('myTeam'), // 標題 我的團隊
+                style: TextStyle(color: AppColors.dialogBlack,
+                    fontSize: UIDefine.fontSize20, fontWeight: FontWeight.w500)),
+
+            _getLine(),
+
+            Row(
+              children: [
+                Flexible(
+                  child: PersonalParamItem(
+                      title: tr('teamRewards'),
+                      value:
+                      NumberFormatUtil().removeTwoPointFormat(levelInfo?.income)),
+                ),
+                Flexible(
+                  child: PersonalParamItem(
+                      title: tr('validInvites'), value: _getValue().toString()),
+                ),
+                Flexible(
+                    child: PersonalParamItem(
+                        title: tr('ALevel'), value: '${levelInfo?.activeDirect}')),
+                Flexible(
+                  child: PersonalParamItem(
+                      title: tr('BCLevel'), value: '${levelInfo?.activeIndirect}'),
+                ),
+              ],
+            ),
+
+            Row(children: [
+              Flexible(
+                  child: PersonalParamItem(
+                      title: tr('teamMember'),
+                      assetImagePath: AppImagePath.myTeamMemberIcon,
+                      onPress: () => _showTeamMemberPage(context))),
+              Flexible(
+                  child: PersonalParamItem(
+                      title: tr('teamContribution'),
+                      assetImagePath: AppImagePath.myTeamContributionIcon,
+                      onPress: () => _showTeamContributionPage(context))),
+              Flexible(
+                  child: PersonalParamItem(
+                      title: tr('teamOrder'),
+                      assetImagePath: AppImagePath.myTeamOrderIcon,
+                      onPress: () => _showTeamOrderPage(context))),
+              Flexible(
+                  child: PersonalParamItem(
+                      title: tr("referral-code'"),
+                      assetImagePath: AppImagePath.myReferralCodeIcon,
+                      onPress: () => _showReferralCodePage(context)))
+            ])
+          ],
+        )
+      )
+    );
+  }
+
+  Widget _getLine() {
+    return Container(
+        margin: EdgeInsets.symmetric(vertical: UIDefine.getScreenWidth(2.7)),
+        width: double.infinity,
+        height: 1,
+        color: AppColors.searchBar
+    );
+  }
+
+  int _getValue() {
+    if (levelInfo != null) {
+      int validInvites = levelInfo!.activeDirect + levelInfo!.activeIndirect;
+      return validInvites;
+    }
+    return 0;
+  }
+
+  void _showTeamMemberPage(BuildContext context) {
+    BaseViewModel().pushPage(context, const TeamMemberPage());
+  }
+
+  void _showTeamContributionPage(BuildContext context) {
+    BaseViewModel().pushPage(context, const TeamContributionPage());
+  }
+
+  void _showTeamOrderPage(BuildContext context) {
+    BaseViewModel().pushPage(context, const TeamOrderPage());
+  }
+
+  void _showReferralCodePage(BuildContext context) {
+    BaseViewModel().pushPage(context, const TeamReferralCodePage());
+  }
+
+}
