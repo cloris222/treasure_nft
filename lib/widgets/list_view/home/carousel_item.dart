a6e8a81c3b6229c7a4ce9c59b948e72fe1d753d8
fangchi <fangchi@trustance.net>
2022-12-28 15:18:34 +0800

    v0.0.11 動畫坑(未完)

--- lib/widgets/list_view/home/carousel_item.dart
+++ lib/widgets/list_view/home/carousel_item.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:treasure_nft_project/constant/ui_define.dart';
 import 'package:treasure_nft_project/models/http/parameter/home_carousel.dart';
@@ -8,38 +7,36 @@ class CarouselItemView extends StatelessWidget {
   const CarouselItemView({
     super.key,
     required this.itemData,
+    required this.index,
   });
 
   final HomeCarousel itemData;
+  final int index;
 
   @override
   Widget build(BuildContext context) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      Expanded(
-        child: Container(
-          alignment: Alignment.topCenter,
-          child: ClipRRect(
-            borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(15), topRight: Radius.circular(15)),
-            child: GraduallyNetworkImage(
-                width: UIDefine.getPixelWidth(340),
-                height: UIDefine.getHeight(),
-                imageUrl: itemData.imageUrl,
-                fit: BoxFit.cover),
-          ),
-        ),
-      ),
-      Container(
-          height: UIDefine.getPixelHeight(50),
-          alignment: Alignment.center,
-          padding: const EdgeInsets.only(left: 15),
-          child: Text(
-            itemData.name,
-            style: TextStyle(
-              fontSize: UIDefine.fontSize16,
-              fontWeight: FontWeight.w400,
-            ),
-          )),
-    ]);
+    return Stack(
+      children: [
+        Container(
+            alignment: Alignment.topCenter,
+            child: ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(15)),
+                child: SizedBox(
+                  width: UIDefine.getPixelWidth(340),
+                  height: UIDefine.getHeight(),
+                  child: GraduallyNetworkImage(
+                      imageUrl: itemData.imageUrl, fit: BoxFit.cover),
+                ))),
+        Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Text('item:$index',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: UIDefine.fontSize24,
+                    fontWeight: FontWeight.w600)))
+      ],
+    );
   }
 }
--- lib/widgets/list_view/home/carousel_listview.dart
+++ lib/widgets/list_view/home/carousel_listview.dart
@@ -1,13 +1,9 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_carousel_slider/carousel_slider.dart';
-import 'package:flutter_carousel_slider/carousel_slider_transforms.dart';
 import 'package:treasure_nft_project/constant/subject_key.dart';
 import 'package:treasure_nft_project/constant/ui_define.dart';
 import 'package:treasure_nft_project/utils/observer_pattern/home/home_observer.dart';
 import 'package:treasure_nft_project/view_models/home/home_main_viewmodel.dart';
-import 'package:treasure_nft_project/constant/global_data.dart';
-
-import '../../../constant/theme/app_style.dart';
 import 'carousel_item.dart';
 
 class CarouselListView extends StatefulWidget {
@@ -46,48 +42,28 @@ class _GetCarouselListView extends State<CarouselListView> {
     super.dispose();
   }
 
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    GlobalData.printLog('precacheImage:');
-  }
-
   Widget createItemBuilder(BuildContext context, int index) {
     return CarouselItemView(
-      itemData: viewModel.homeCarouselList[index],
-    );
+        itemData: viewModel.homeCarouselList[index], index: index);
   }
 
   @override
   Widget build(BuildContext context) {
-    CarouselSliderController sliderController = CarouselSliderController();
-    GlobalKey<_GetCarouselListView> sliderKey = GlobalKey();
     double itemHeight = UIDefine.getWidth() * 0.8;
-
+    itemHeight = itemHeight > 430 ? itemHeight : 430;
     return SizedBox(
-      height: itemHeight > 430 ? itemHeight : 430,
-      child: Container(
-        /// 0px 6px 5px rgba(9, 9, 9, 0.15);
-        decoration: AppStyle().styleShadowBorderBackground(
-            borderWidth: 0,
-            radius: 20,
-            borderColor: Colors.transparent,
-            offsetX: 0,
-            offsetY: 6,
-            blurRadius: 5,
-            shadowColor: const Color(0xFF090909).withOpacity(0.15)),
-        child: CarouselSlider.builder(
-          key: sliderKey,
-          unlimitedMode: true,
-          controller: sliderController,
-          slideBuilder: (index) {
-            return createItemBuilder(context, index);
-          },
-          slideTransform: const DefaultTransform(),
-          itemCount: viewModel.homeCarouselList.length,
-          enableAutoSlider: true,
-          autoSliderTransitionTime: const Duration(seconds: 1),
-        ),
+      height: itemHeight,
+      child: Swiper(
+        loop: true,
+        itemWidth: itemHeight * 0.8,
+        itemHeight: itemHeight * 0.8,
+        duration: 600,
+        itemBuilder: (BuildContext context, int index) {
+          return createItemBuilder(context, index);
+        },
+        curve: Curves.easeInToLinear,
+        itemCount: viewModel.homeCarouselList.length,
+        layout: SwiperLayout.STACK,
       ),
     );
   }
