d9b3310757b0d99d360e376ca257ca4b8dc2e164
fangchi <fangchi@trustance.net>
2023-01-07 16:43:06 +0800

    v0.0.11 init font modify

---
+++ lib/constant/enum/style_enum.dart
@@ -0,0 +1,1 @@
+enum CustomTextFamily { PosteramaText, Posterama1927 }
--- lib/utils/custom_text_style.dart
+++ lib/utils/custom_text_style.dart
@@ -1,14 +1,18 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
+
 import '../constant/theme/app_colors.dart';
 
 import '../constant/enum/style_enum.dart';
 
 class CustomTextStyle {
-  TextStyle getBaseStyle(
+  const CustomTextStyle._();
+
+  static TextStyle getBaseStyle(
       {Color color = AppColors.textBlack,
       double? fontSize,
       FontWeight? fontWeight,
-      CustomTextFamily fontFamily = CustomTextFamily.PosteramaText}) {
+      CustomTextFamily fontFamily = CustomTextFamily.PosteramaText,
+      FontStyle? fontStyle}) {
     return TextStyle(
         color: color,
         fontSize: fontSize,
@@ -17,6 +21,7 @@ class CustomTextStyle {
             ? (fontWeight.index > FontWeight.w500.index
                 ? FontWeight.w500
                 : fontWeight)
-            : null);
+            : null,
+        fontStyle: fontStyle);
   }
 }
