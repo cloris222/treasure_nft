f881d4356e402eee451446a6a4b7d66623c39a86
fangchi <fangchi@trustance.net>
2023-02-04 15:50:09 +0800

    文字粗體

--- lib/utils/app_text_style.dart
+++ lib/utils/app_text_style.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:treasure_nft_project/constant/global_data.dart';
 import 'package:treasure_nft_project/constant/ui_define.dart';
+import 'package:treasure_nft_project/view_models/base_view_model.dart';
 
 import '../constant/theme/app_colors.dart';
 
@@ -8,6 +9,13 @@ import '../constant/enum/style_enum.dart';
 
 class AppTextStyle {
   const AppTextStyle._();
+
+  static bool isSystemBold() {
+    bool isSystemBold =
+        MediaQuery.of(BaseViewModel().getGlobalContext()).boldText;
+    GlobalData.printLog('isSystemBold:$isSystemBold');
+    return isSystemBold;
+  }
 
   static TextStyle getBaseStyle(
       {Color color = AppColors.textBlack,
@@ -23,8 +31,7 @@ class AppTextStyle {
         fontWeight: fontWeight != null
 
             ///MARK: 在release mode才加判斷
-            ? (fontWeight.index > FontWeight.w500.index &&
-                    !GlobalData.isDebugMode()
+            ? (fontWeight.index > FontWeight.w500.index && isSystemBold()
                 ? FontWeight.w500
                 : fontWeight)
             : null,
