e2776f38aaae11d6459c554a128f7009ca62d2e4
Jack <jack@locas.com.tw>
2023-06-02 17:32:56 +0800

    加入HTML套件 & 標籤和圖片加上圓角

--- lib/views/announcement/announcement_item_view.dart
+++ lib/views/announcement/announcement_item_view.dart
@@ -9,6 +9,8 @@ import '../../models/http/parameter/announce_data.dart';
 import '../../view_models/announcement/announcement_view_model.dart';
 import 'announcement_detail_page.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:html/parser.dart';
+
 
 
 /// 公告欄 ItemView
@@ -42,49 +44,58 @@ class _AnnouncementItemViewItemView extends State<AnnouncementItemView> {
         onTap: () => BaseViewModel().pushPage(
             context, AnnouncementDetailPage(data: data, viewModel: viewModel)),
         child: Container(
-      height: UIDefine.getPixelHeight(120),
-      width: UIDefine.getWidth(),
-      margin: const EdgeInsets.fromLTRB(8, 3, 8, 3),
-      child: Row(children: [
-        Expanded(child:
-        Column(crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(data.title,
-                  style: TextStyle(fontSize: UIDefine.fontSize16)),
-              SizedBox(height: UIDefine.getPixelHeight(8)),
-              Row(children:buildTagItem(data.tagId)),
-              SizedBox(height: UIDefine.getPixelHeight(8)),
-              Text(getTime(data.startAt),
-                  style: TextStyle(fontSize: UIDefine.fontSize12)),
-            ])),
+          height: UIDefine.getPixelHeight(120),
+          width: UIDefine.getWidth(),
+          margin: const EdgeInsets.fromLTRB(8, 3, 8, 3),
+          child: Row(children: [
+            Expanded(child:
+            Column(crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(data.title,
+                      style: TextStyle(fontSize: UIDefine.fontSize16)),
+                  SizedBox(height: UIDefine.getPixelHeight(8)),
+                  Row(children:buildTagItem(data.tagId)),
+                  SizedBox(height: UIDefine.getPixelHeight(8)),
+                  Text(getTime(data.startAt),
+                      style: TextStyle(fontSize: UIDefine.fontSize12)),
+                ])),
 
-        CachedNetworkImage(
-          imageUrl: data.bannerMbUrl,
-          memCacheWidth: 480,
-          cacheManager: CacheManager(
-            Config("flutterCampus", stalePeriod: const Duration(minutes: 5)),
-          ),
-        ),
+            ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(12)),
+              child:CachedNetworkImage(
+                imageUrl: data.bannerMbUrl,
+                memCacheWidth: 160,
+                errorWidget: (BuildContext context,
+                    String url,
+                    dynamic error)=>const SizedBox(),
+                cacheManager: CacheManager(
+                  Config("flutterCampus", stalePeriod: const Duration(minutes: 5)),
+                ),
+              ),
+            )
 
-      ]),
-    ));
+
+          ]),
+        ));
   }
 
   List<Widget> buildTagItem(List<String> tagId) {
     List<Widget> widgets = [];
     for (String id in tagId) {
-      widgets.add(Container(
-        alignment: Alignment.bottomCenter,
-        margin: EdgeInsets.only(right: UIDefine.getPixelWidth(10)),
-        padding: const EdgeInsets.fromLTRB(6, 1, 6, 2),
-        height: UIDefine.getPixelHeight(24),
-        color: HexColor(viewModel.getTagColor(id)),
-        child: Text(viewModel.getTagText(id),
-            style: TextStyle(
-                fontSize: UIDefine.fontSize12,
-                color: AppColors.dialogBlack,
-            )),
-      ));
+      widgets.add( ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(6)),
+          child:Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.fromLTRB(6, 1, 6, 1),
+            height: UIDefine.getPixelHeight(24),
+            color: HexColor(viewModel.getTagColor(id)),
+            child: Text(viewModel.getTagText(id),
+                style: TextStyle(
+                  fontSize: UIDefine.fontSize12,
+                  color: AppColors.dialogBlack,
+                )),
+          )));
+      widgets.add(SizedBox(width: UIDefine.getPixelWidth(10)));
     }
     return widgets;
   }
