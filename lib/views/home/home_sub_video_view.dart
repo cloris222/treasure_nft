37a83769dd501313dfa8c82a113b469c4905affd
fangchi <lily@locas.com.tw>
2023-07-27 12:21:20 +0800

    工單914 首頁video根據影片更換圖片

--- lib/views/home/home_sub_video_view.dart
+++ lib/views/home/home_sub_video_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:format/format.dart';
+import 'package:treasure_nft_project/utils/language_util.dart';
 import 'package:video_player/video_player.dart';
 import 'package:chewie/chewie.dart';
+import '../../constant/enum/setting_enum.dart';
 import '../../constant/theme/app_image_path.dart';
 import '../../constant/ui_define.dart';
 import '../../models/http/http_setting.dart';
@@ -19,7 +22,7 @@ class _HomeSubVideoViewState extends State<HomeSubVideoView> {
 
   @override
   void initState() {
-    _videoController = VideoPlayerController.network(HttpSetting.homeAdUrl)
+    _videoController = VideoPlayerController.network(_getVideoPath())
       ..initialize().then((value) {
         setState(() {
           _playerController = ChewieController(
@@ -50,9 +53,7 @@ class _HomeSubVideoViewState extends State<HomeSubVideoView> {
   Widget build(BuildContext context) {
     return SizedBox(
         width: UIDefine.getWidth(),
-        height: _videoController.value.isInitialized
-            ? (UIDefine.getWidth() / _videoController.value.aspectRatio)
-            : UIDefine.getPixelHeight(200),
+        height: _videoController.value.isInitialized ? (UIDefine.getWidth() / _videoController.value.aspectRatio) : UIDefine.getPixelHeight(200),
         child: Container(
           child: _videoController.value.isInitialized
               ? _videoController.value.isPlaying
@@ -66,18 +67,13 @@ class _HomeSubVideoViewState extends State<HomeSubVideoView> {
                       child: InkWell(
                           onTap: _onStart,
                           child: Stack(alignment: Alignment.center, children: [
-                            Transform.scale(
-                              scaleX: 1.1,
-                             child:Image.asset(AppImagePath.videoPoster)),
-
+                            Transform.scale(scaleX: 1.1, child: Image.asset(_getImagePath())),
                             Opacity(
                                 opacity: 0.87,
                                 child: CircleAvatar(
                                     radius: UIDefine.getScreenWidth(8),
                                     backgroundColor: Colors.white,
-                                    child: Icon(Icons.play_arrow,
-                                        color: Colors.black,
-                                        size: UIDefine.getScreenWidth(8))))
+                                    child: Icon(Icons.play_arrow, color: Colors.black, size: UIDefine.getScreenWidth(8))))
                           ])))
               : Container(),
         ));
@@ -88,7 +84,45 @@ class _HomeSubVideoViewState extends State<HomeSubVideoView> {
   }
 
   void _onStart() {
-    _videoController.seekTo(const Duration(seconds: 0)).then(
-        (value) => _videoController.play().then((value) => setState(() {})));
+    _videoController.seekTo(const Duration(seconds: 0)).then((value) => _videoController.play().then((value) => setState(() {})));
+  }
+
+  String _getImagePath() {
+    String lang;
+    switch (LanguageUtil.getSettingLanguageType()) {
+      case LanguageType.Arabic:
+        lang = "ar";
+        break;
+      case LanguageType.Vietnamese:
+        lang = "vi";
+        break;
+      case LanguageType.Turkish:
+        lang = "tr";
+        break;
+      default:
+        lang = "en";
+        break;
+    }
+
+    return format(AppImagePath.videoPoster, {"lang": lang});
+  }
+
+  String _getVideoPath() {
+    String lang;
+    switch (LanguageUtil.getSettingLanguageType()) {
+      case LanguageType.Arabic:
+        lang = "ar";
+        break;
+      case LanguageType.Vietnamese:
+        lang = "vn";
+        break;
+      case LanguageType.Turkish:
+        lang = "tr";
+        break;
+      default:
+        lang = "en";
+        break;
+    }
+    return format(HttpSetting.homeAdUrl, {"lang": lang});
   }
 }
